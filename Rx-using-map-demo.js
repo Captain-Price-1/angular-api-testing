011df4b71b290e3cc713c12f346b2728dcf73955
Anas Parwez <anas@slikr.com.au>
2022-10-18 15:13:49 +0530

    Added file Rx-using-map-demo

---
+++ Rx-using-map-demo.js
@@ -0,0 +1,29 @@
+// FromEvent is an operator that is different and it returns an observable
+const { fromEvent } = Rx;
+const { map } = RxOperators;
+
+const input = document.createElement("input");
+const container = document.querySelector(".container");
+container.appendChild(input);
+
+const observable = fromEvent(input, "input").pipe(
+  map((event) => event.target.value),
+  map((value) => parseInt(value)),
+  map((value) => {
+    if (isNaN(value)) {
+      throw new Error("Enter a number");
+    }
+    return value;
+  })
+);
+
+observable.subscribe({
+  next(value) {
+    //value when the observable completes without any errors
+    console.log(`Your value is ${value}`);
+  },
+  error(err) {
+    // anything wrong occus inside the observable goes into error here
+    console.error("Some Error Occured", err.message);
+  },
+});
